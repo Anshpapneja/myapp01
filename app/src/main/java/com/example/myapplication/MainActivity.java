e008e2d982715b56177ad40cf34061e42eb37163
Anshpapneja <Anshpapneja01@gmail.com>
2023-05-08 00:38:55 +0530

    commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+     TextView txt;
+     Button btn;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txt = findViewById(R.id.textView);
+        btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random random = new Random();
+                int val = random.nextInt(1000);
+                txt .setText(Integer.toString(val));
+            }
+        });
+
+    }
+}
